a260d74f1e815ce95db7f277c61587093984c52f
Josh Pencheon <josh.pencheon@googlemail.com>
2011-07-31 17:18:47 +0100

    Fix broken tests

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "format_price should fill in any trailing zeros" do
-    assert_equal '10.20', format_price(10.2)
-    assert_equal '10.00', format_price(10.0)
-    assert_equal '10.00', format_price(10)
+    assert_equal '£10.20', format_price(10.2)
+    assert_equal '£10.00', format_price(10.0)
+    assert_equal '£10.00', format_price(10)
   end
   
   test "format_price should by default drop the currency symbol" do
-    assert_equal '123.46', format_price(123.456)
+    assert_equal '£123.46', format_price(123.456)
   end
   
-  test "format_price should by include the currency symbol with a truthy second argument" do
+  test "format_price should not include the currency symbol with a truthy second argument" do
     assert_equal '£123.46', format_price(123.456, true)
   end
 end
