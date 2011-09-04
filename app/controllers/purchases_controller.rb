06c7caef39833fb17fdfd63df74e28f4267468a4
Josh Pencheon <josh.pencheon@googlemail.com>
2011-09-04 16:27:47 +0100

    Don't use flash for XHR when updating basket.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,29 +3,12 @@ class PurchasesController < ApplicationController
   
   before_filter :find_basket
   
-  # def create
-  #   product = Product.find_or_create_from_params(params[:product])
-  #   
-  #   @purchase = product.purchases.build(params[:purchase])
-  #   if product.id && @purchase.save
-  #     flash[:notice] = "Saved!"
-  #     @purchase_created = true
-  #   else
-  #     flash[:error] = "Invalid!"
-  #   end
-  #   
-  #   respond_to do |format|
-  #     format.html { redirect_to @purchase.basket }
-  #     format.js { render Basket.find(params[:purchase][:basket_id]) }
-  #   end
-  # end
-  
   def create
     product = Product.find_or_create_from_params(params[:product])
     purchase = @basket.purchases.create_or_update(params[:purchase].merge(:product_id => product.id))
     
-    if product.new_record? || purchase.new_record?
-      flash[:error] = "'Error saving' - #{product.new_record?}, #{purchase.new_record?}"
+    if (product.new_record? || purchase.new_record?) && !request.xhr?
+      flash[:error] = 'Error saving!'
     else
       flash[:notice] = 'Saved!'
     end
